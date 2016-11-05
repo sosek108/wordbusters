c582a723bf0bbdafdcd964114182ffe636470395
Hubert S <sosek108@gmail.com>
2016-11-05 13:17:42 +0100

    gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
+var minify = require('gulp-minify');
 
 gulp.task('css', function () {
-
     return gulp.src('./assets/postcss/main.css')
         .pipe(postcss([
             require('postcss-partial-import'),
@@ -11,6 +11,18 @@ gulp.task('css', function () {
         ]))
         .pipe(gulp.dest('./wwwroot/css/'));
 });
+
+gulp.task('js', function() {
+    gulp.src('assets/js/*.js')
+    .pipe(minify({
+        ext:{
+          min: '.js'
+        }
+    }))
+    .pipe(gulp.dest('./wwwroot/js/'))
+})
+
 gulp.task('watch', function() {
     gulp.watch('assets/postcss/*', ['css'])
-})
+    gulp.watch('assets/js/*', ['js'])
+});
